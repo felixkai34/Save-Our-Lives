8a1b60a3ca87a168cd805e9d29d7625f8899ccd7
felix <aungnay123234@gmail.com>
2024-08-09 11:53:47 +0630

    added new post

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route , Routes } from 'react-router-dom'
+import { Link, Route , Routes } from 'react-router-dom'
 import './App.css'
 import Home from './assets/pages/Home'
 import Detail from './assets/pages/Detail'
@@ -12,8 +12,9 @@ function App() {
 				<Route path="/:Id" element={<Detail/>} />
     		</Routes>
 
-			<div className=' footer'>
-				<h1 className=' text-center text-white py-5'> Contents credit to the rightful owners </h1>
+			<div className=' footer flex justify-center items-center flex-wrap'>
+				<h1 className=' text-center text-white py-5'> Contents credit to the rightful owners. </h1>
+				<Link className=' text-white underline font-bold ml-3' to={"https://www.t.me/ANTheGod"} target='_blank'> Contact to developer</Link>
 			</div>
 		</>
 	)
